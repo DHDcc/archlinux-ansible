6d83c3e55006b7a152375fe46bcc2993dfec3fee
Tomo <github.gas371@slmail.me>
2025-01-03 18:29:55 +0100

    refactor(fonts_and_themes): change how onetime.sh works

--- ansible/fonts_and_themes/files/onetime.sh
+++ ansible/fonts_and_themes/files/onetime.sh
@@ -1,21 +1,33 @@
 #!/usr/bin/env bash
 
-wallpaperPath="$HOME/.config/swww/wallpapers/ggirl.png"
-hyprlandConf="$HOME/.config/hypr/hyprland.conf"
-lineRemoved="/exec-once = \$HOME\/.scripts\/onetime.sh/d"
-
-removeScript(){
-	local scriptName="${0##*/}"
-
-	if ls "$HOME/.scripts/${scriptName}"; then
-		rm -f "$HOME/.scripts/${scriptName}"
-	else 
-		exit 1 
-	fi
+setWallpaper(){
+  local wallpaperPath="$HOME/.config/swww/wallpapers/ggirl.png"
+  swww img "${wallpaperPath}" --transition-fps 120 --transition-type fade --transition-duration 1
 }
 
-swww img "${wallpaperPath}" --transition-fps 170 --transition-type fade --transition-duration 1
-kitty -e  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
-sed -i "${lineRemoved}" "${hyprlandConf}"
+zsh4humans(){
+  local zsh4humansURL="https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install"
+  kitty -e  sh -c "$(curl -fsSL ${zsh4humansURL})"
+}
 
+removeScriptFromHyprlandConfigFile(){
+  local hyprlandConfigFile="$HOME/.config/hypr/hyprland.conf"
+  local lineRemoved="/exec-once = \$HOME\/.scripts\/onetime.sh/d"
+
+  sed -i "${lineRemoved}" "${hyprlandConfigFile}"
+}
+
+removeScript(){
+  local scriptName="${0##*/}"
+
+  if ls "$HOME/.scripts/${scriptName}"; then
+	rm -f "$HOME/.scripts/${scriptName}"
+  else 
+ 	exit 1 
+  fi
+}
+
+setWallpaper
+zsh4humans
+removeScriptFromHyprlandConfigFile
 removeScript
